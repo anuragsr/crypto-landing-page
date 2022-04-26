757e20ca61a1af87ca85c976c555d29dea3de860
anuragsr <anurag.131092@gmail.com>
2022-04-26 19:10:28 +0530

    Added ticker skeleton

--- src/js/SinglePage.js
+++ src/js/SinglePage.js
@@ -1,3 +1,4 @@
+import * as $ from 'jquery'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Parallax from 'parallax-js'
@@ -19,13 +20,12 @@ export default class SinglePage {
   }
   init2D(){
     // Parallax scene for first section
-    const scene = document.getElementById('parallax-scene')
-    , parallaxInstance = new Parallax(scene, {
+    new Parallax(document.getElementById('parallax-scene'), {
       relativeInput: true, hoverOnly: true,
       inputElement: document.getElementById('section1')
     })
 
-    // Scrolltrigger timeline
+    // Scrolltrigger timelines
     const markers = true
     new gsap.timeline({
       scrollTrigger: {
@@ -93,6 +93,46 @@ export default class SinglePage {
         },
       }
     })
+
+    // Ticker
+    const $tickerWrapper = $(".ticker-wrapper")
+      , $list = $tickerWrapper.find("ul.list")
+      , $clonedList = $list.clone()
+      , duration = 10
+
+    let listWidth = 30
+
+    $list.find("li").each(function(i) {
+      listWidth += $(this, i).outerWidth(true);
+    })
+    $list.add($clonedList).css({ width: listWidth + "px" })
+    $clonedList.addClass("cloned").appendTo($tickerWrapper)
+
+    new gsap
+      .timeline({ repeat: -1 })
+      .fromTo($list,
+        { rotation: 0.01, x: 0 },
+        { duration, force3D: true, x: -listWidth, ease: "none" }
+      , 0)
+      .fromTo($clonedList,
+        { rotation: 0.01, x: listWidth },
+        { duration, force3D: true, x: 0, ease: "none" }
+      , 0)
+      .set($list, { force3D: true, rotation: 0.01, x: listWidth })
+      .to($clonedList, {
+        duration,
+        force3D: true,
+        rotation: 0.01,
+        x: -listWidth,
+        ease: "none"
+      }, duration)
+      .to($list, {
+        duration,
+        force3D: true,
+        rotation: 0.01,
+        x: 0,
+        ease: "none"
+      }, duration)
   }
   init3D(){
     // THREE.js scene
